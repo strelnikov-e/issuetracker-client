cac6ef2035e8b6d6bbd94875ddd778379362c132
strelnikov <evstrelnikov@gmail.com>
2023-08-13 16:40:20 +0300

    change address for railway deploy

--- src/components/Constants.js
+++ src/components/Constants.js
@@ -1,3 +1,3 @@
-// export const address = "http://localhost:8080"
 export const address = "https://issuetracker-production.up.railway.app"
+// export const address = "http://localhost:8080"
 // export const address = ""
